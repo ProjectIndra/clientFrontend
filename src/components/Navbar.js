0aad5f29103a265a25fbf988b3e0e0eedad596e9
HetarthJain <hetarth.jain@gmail.com>
2025-03-04 01:03:54 +0530

    ui-home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,33 +2,35 @@ import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 
 function Navbar() {
-  return (
-    <div className='navbar'>
-    <nav>
-      <div className="menu-icon">
-            <span className="fas fa-bars"></span>
-         </div>
-         <div className="logo">
-            Indra
-         </div>
-         <div className="nav-items">
-            <li><Link to="/home">Home</Link></li>
-            <li><Link to="/client/services">My Services</Link></li>
-            <li><Link to="/providersList">Providers</Link></li>
-         </div>
-         <div className="search-icon">
-            <span className="fas fa-search"></span>
-         </div>
-         <div className="cancel-icon">
-            <span className="fas fa-times"></span>
-         </div>
-         <form action="#">
-            <input type="search" className="search-data" placeholder="Search" required />
-            <button type="submit" className="fas fa-search"></button>
-         </form>
+   return (
+      <div className='navbar'>
+         <nav>
+            <div className="menu-icon">
+               <span class="material-symbols-outlined">
+                  menu
+               </span>
+            </div>
+            <div className="logo">
+               Indra
+            </div>
+            <div className="nav-items">
+               <li><Link to="/home">Home</Link></li>
+               <li><Link to="/client/services">My Services</Link></li>
+               <li><Link to="/providersList">Providers</Link></li>
+            </div>
+            <div className="search-icon">
+               <span className="fas fa-search"></span>
+            </div>
+            <div className="cancel-icon">
+               <span className="fas fa-times"></span>
+            </div>
+            <form action="#">
+               <input type="search" className="search-data" placeholder="Search" required />
+               <button type="submit" className="fas fa-search"></button>
+            </form>
          </nav>
-    </div>
-  );
+      </div>
+   );
 }
 
 export default Navbar;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,21 @@
 import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import "../css/Home.css";
-import axios from "axios";
 
 function Home() {
 
     return (
         <div>
             <Navbar />
-            <h1>Welcome To project CodeName:Indra</h1>
+            <div className="home-container">
+                <div className="h21">
+                    <p className="p21">Turn your idle computer into a <strong>cloud provider</strong></p>
+                </div>
+                <div className="h22">
+                    <p className="p22">Use affordable and <strong>distributed consumer VMs</strong></p>
+
+                </div>
+            </div>
         </div>
     );
 }

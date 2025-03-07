4d2be300ec7afa16efc92f8db1b41a14c42bf55d
HetarthJain <hetarth.jain@gmail.com>
2025-03-07 08:52:31 +0530

    homepage done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,28 +6,24 @@ function Navbar() {
       <div className='navbar'>
          <nav>
             <div className="menu-icon">
-               <span class="material-symbols-outlined">
-                  menu
-               </span>
+               <img src='/img/menu.png' alt='menuIcon'>
+               </img>
             </div>
             <div className="logo">
                Indra
             </div>
-            <div className="nav-items">
+            {/* <div className="nav-items">
                <li><Link to="/home">Home</Link></li>
                <li><Link to="/client/services">My Services</Link></li>
                <li><Link to="/providersList">Providers</Link></li>
-            </div>
-            <div className="search-icon">
-               <span className="fas fa-search"></span>
-            </div>
-            <div className="cancel-icon">
-               <span className="fas fa-times"></span>
-            </div>
+            </div> */}
             <form action="#">
-               <input type="search" className="search-data" placeholder="Search" required />
-               <button type="submit" className="fas fa-search"></button>
+               <input type="search" className="search-data" placeholder="Search for providers, services and more" required />
+               <button type="submit">Search</button>
             </form>
+            <div className="profile-details">
+               <img src='/img/user.png' alt='profilePic'></img>
+            </div>
          </nav>
       </div>
    );
